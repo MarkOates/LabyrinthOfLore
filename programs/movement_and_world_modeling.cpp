6eb5acdd0c199560bdfc88bbc85f5756937ae50c
Mark Oates <markoates0@gmail.com>
2019-12-14 15:01:10 -0500

    Add more rendering and update movement and modeling program

--- programs/movement_and_world_modeling.cpp
+++ programs/movement_and_world_modeling.cpp
@@ -1,7 +1,27 @@
 #include <allegro5/allegro.h>
 
+#include <LabyrinthOfLore/WorldMap/BasicRenderer.hpp>
+
+
+bool active = false;
+
 int main(int argc, char **argv)
 {
+   if (active)
+   {
+      al_init();
+
+      ALLEGRO_DISPLAY *display = al_create_display(1920, 1080);
+
+      LabyrinthOfLore::WorldMap::BasicRenderer tile_map_renderer;
+
+      tile_map_renderer.render();
+
+      al_flip_display();
+
+      sleep(2);
+   }
+
    return 0;
 }
 
--- src/LabyrinthOfLore/WorldMap/BasicRenderer.cpp
+++ src/LabyrinthOfLore/WorldMap/BasicRenderer.cpp
@@ -3,6 +3,8 @@
 #include <LabyrinthOfLore/WorldMap/BasicRenderer.hpp>
 #include <allegro5/allegro_primitives.h>
 #include <LabyrinthOfLore/WorldMap/Tile.hpp>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_color.h>
 
 
 namespace LabyrinthOfLore
@@ -30,10 +32,24 @@ al_init_primitives_addon();
 
 void BasicRenderer::render()
 {
+int tile_width = 32;
+int tile_height = 32;
+
+float bottom_height = 0;
+float top_height = 100;
+ALLEGRO_COLOR tile_color = al_color_name("white");
+
 for (unsigned y=0; y<tile_map.get_height(); y++)
   for (unsigned x=0; x<tile_map.get_width(); x++)
    {
       LabyrinthOfLore::WorldMap::Tile tile = tile_map.get_tile(x, y);
+      al_draw_filled_rectangle(
+         x * tile_width,
+         y * tile_height,
+         x * tile_width + tile_width,
+         y * tile_height + tile_height,
+         tile_color
+      );
    }
 return;
 
