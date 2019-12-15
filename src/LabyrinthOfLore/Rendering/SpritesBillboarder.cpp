bc712842f13d27b99a60f8dc0f93a1da6153107d
Mark Oates <markoates0@gmail.com>
2019-12-15 09:54:38 -0500

    Remove commented and no longer used strategy to calc billboarding transformation

--- src/LabyrinthOfLore/Rendering/SpritesBillboarder.cpp
+++ src/LabyrinthOfLore/Rendering/SpritesBillboarder.cpp
@@ -26,26 +26,13 @@ SpritesBillboarder::~SpritesBillboarder()
 
 void SpritesBillboarder::process()
 {
-
-//ALLEGRO_TRANSFORM transform;
-//al_identity_transform(&transform);
-
-//camera_placement.build_reverse_transform(&transform);
-
 AllegroFlare::vec3d camera_rotation = camera_placement.rotation;
-
-AllegroFlare::vec3d reverse_camera_rotation(
-   camera_rotation.x, // + 0.5, //AllegroFlare::FULL_ROTATION/2,
-   camera_rotation.y, // + 0.5, //AllegroFlare::FULL_ROTATION/2,
-   camera_rotation.z //  + 0.5 //AllegroFlare::FULL_ROTATION/2
-);
-
 
 for (auto &entity : entities)
 {
    if (entity->get_billboard_at_camera())
    {
-      entity->get_placement_ref().rotation = reverse_camera_rotation;
+      entity->get_placement_ref().rotation = camera_rotation;
    }
 }
 return;
