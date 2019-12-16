2278df9d30db00003f7eca598a06dcffd37bb4a1
Mark Oates <markoates0@gmail.com>
2019-12-16 00:56:26 -0500

    Disable saving surfaces for debugging

--- programs/picking.cpp
+++ programs/picking.cpp
@@ -158,10 +158,9 @@ int main(int argc, char **argv)
                LabyrinthOfLore::Rendering::PickingBufferRenderer picking_buffer_renderer(&picking_buffer, camera_placement, entities, &clamped_color_shader);
                picking_buffer_renderer.render();
 
-               al_save_bitmap("/Users/markoates/Repos/LabyrinthOfLore/tmp/regular_render.png", render_surface);
-               al_save_bitmap("/Users/markoates/Repos/LabyrinthOfLore/tmp/picking_render.png", picking_buffer.get_surface_render());
-
-               shutdown_program = true;
+               //al_save_bitmap("/Users/markoates/Repos/LabyrinthOfLore/tmp/regular_render.png", render_surface);
+               //al_save_bitmap("/Users/markoates/Repos/LabyrinthOfLore/tmp/picking_render.png", picking_buffer.get_surface_render());
+               //shutdown_program = true;
 
                al_flip_display();
             }
