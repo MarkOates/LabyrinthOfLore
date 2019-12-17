d3b994730b4dd4a86aff8e5c2fc8669bdaac80e3
Mark Oates <markoates0@gmail.com>
2019-12-16 21:45:17 -0500

    Get first real EntityTileMapCollisionStepper passing

--- tests/LabyrinthOfLore/Physics/EntityTileMapCollisionStepperTest.cpp
+++ tests/LabyrinthOfLore/Physics/EntityTileMapCollisionStepperTest.cpp
@@ -28,3 +28,25 @@ TEST(LabyrinthOfLore_Physics_EntityTileMapCollisionStepperTest, process_step__wo
    SUCCEED();
 }
 
+TEST(LabyrinthOfLore_Physics_EntityTileMapCollisionStepperTest,
+      with_an_entity__process_step__steps_the_entity_forward_by_its_velocity)
+{
+   LabyrinthOfLore::WorldMap::TileMap tile_map;
+   tile_map.resize(10, 10, LabyrinthOfLore::WorldMap::Tile(0, 10.0));
+   LabyrinthOfLore::Entity::Base entity = LabyrinthOfLore::Entity::Base();
+   std::vector<LabyrinthOfLore::Entity::Base*> entities = { &entity };
+
+   entity.get_placement_ref().position = AllegroFlare::vec3d(0.0, 0.0, 0.0);
+   entity.get_velocity_ref().position = AllegroFlare::vec3d(0.3, 0.4, 0.2);
+
+   LabyrinthOfLore::Physics::EntityTileMapCollisionStepper entity_tile_map_collision_stepper(tile_map, entities);
+   entity_tile_map_collision_stepper.process_step();
+
+   allegro_flare::placement3d actual_placement = entity.get_placement_ref();
+   EXPECT_EQ(0.3f, actual_placement.position.x);
+   EXPECT_EQ(0.4f, actual_placement.position.y);
+   EXPECT_EQ(0.2f, actual_placement.position.z);
+
+   entity_tile_map_collision_stepper.process_step();
+}
+
