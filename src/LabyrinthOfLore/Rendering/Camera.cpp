1d85cfd8746a83b9c283ff11bbc7fde19736865d
Mark Oates <markoates0@gmail.com>
2019-12-17 22:58:02 -0500

    Get working tests on Camera

--- src/LabyrinthOfLore/Rendering/Camera.cpp
+++ src/LabyrinthOfLore/Rendering/Camera.cpp
@@ -2,6 +2,7 @@
 
 #include <LabyrinthOfLore/Rendering/Camera.hpp>
 #include <allegro5/allegro.h>
+#include <allegro_flare/placement3d.h>
 
 
 namespace LabyrinthOfLore
@@ -24,6 +25,18 @@ Camera::~Camera()
 void Camera::start_projection(ALLEGRO_BITMAP* surface)
 {
 if (!surface) throw std::runtime_error("[Camera::start_projection] error: cannot set the projection on nullptr surface");
+
+al_set_render_state(ALLEGRO_DEPTH_TEST, 1);
+al_set_render_state(ALLEGRO_WRITE_MASK, ALLEGRO_MASK_DEPTH | ALLEGRO_MASK_RGBA);
+al_clear_depth_buffer(1);
+
+ALLEGRO_TRANSFORM transform;
+allegro_flare::placement3d camera_view(0, 0, 2);
+camera_view.build_reverse_transform(&transform);
+
+float aspect_ratio = (float)al_get_bitmap_height(surface) / al_get_bitmap_width(surface);
+al_perspective_transform(&transform, -1, aspect_ratio, 1, 1, -aspect_ratio, 100);
+al_use_projection_transform(&transform);
 
 return;
 
--- tests/LabyrinthOfLore/Rendering/CameraTest.cpp
+++ tests/LabyrinthOfLore/Rendering/CameraTest.cpp
@@ -10,13 +10,20 @@
 #include <LabyrinthOfLore/Rendering/Camera.hpp>
 
 
+#include <allegro5/allegro_color.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
+
+
 class LabyrinthOfLore_Rendering_CameraTest : public ::testing::Test
 {
 protected:
    ALLEGRO_DISPLAY *display;
+   float near_threshold;
 
    LabyrinthOfLore_Rendering_CameraTest()
       : display(nullptr)
+      , near_threshold(0.0001f)
    {
    }
 
@@ -24,6 +31,13 @@ protected:
    {
       ASSERT_EQ(false, al_is_system_installed());
       ASSERT_EQ(true, al_init());
+      ASSERT_EQ(true, al_init_primitives_addon());
+      ASSERT_EQ(true, al_init_image_addon());
+
+      //al_set_new_display_option(ALLEGRO_DEPTH_SIZE, 32, ALLEGRO_SUGGEST);
+      //al_set_new_display_flags(ALLEGRO_OPENGL | ALLEGRO_PROGRAMMABLE_PIPELINE);
+      //ASSERT_EQ(ALLEGRO_OPENGL, al_get_new_display_flags() & ALLEGRO_OPENGL);
+      //ASSERT_EQ(ALLEGRO_PROGRAMMABLE_PIPELINE, al_get_new_display_flags() & ALLEGRO_PROGRAMMABLE_PIPELINE);
 
       ALLEGRO_DISPLAY *display = al_create_display(800, 600);
       ASSERT_NE(nullptr, display);
@@ -33,6 +47,32 @@ protected:
    {
       if (display) al_destroy_display(display);
       al_uninstall_system();
+   }
+
+   void draw_targets()
+   {
+      float x = 0;
+      float y = 0;
+      float size = 0.1;
+      al_draw_filled_rectangle(-size+x, -size+y, size+x, size+y, al_color_name("white"));
+
+      x = 1;
+      y = 0;
+      al_draw_filled_rectangle(-size+x, -size+y, size+x, size+y, al_color_name("yellow"));
+
+      x = -1;
+      y = 0;
+      al_draw_filled_rectangle(-size+x, -size+y, size+x, size+y, al_color_name("orange"));
+   }
+
+   void ASSERT_CENTRAL_PIXEL_MATCHES(ALLEGRO_BITMAP *surface, ALLEGRO_COLOR expected_color)
+   {
+      ASSERT_NE(nullptr, surface);
+      ALLEGRO_COLOR actual_color = al_get_pixel(surface, al_get_bitmap_width(surface)/2, al_get_bitmap_height(surface)/2);
+      EXPECT_NEAR(expected_color.r, actual_color.r, near_threshold);
+      EXPECT_NEAR(expected_color.g, actual_color.g, near_threshold);
+      EXPECT_NEAR(expected_color.b, actual_color.b, near_threshold);
+      EXPECT_NEAR(expected_color.a, actual_color.a, near_threshold);
    }
 };
 
@@ -61,7 +101,29 @@ TEST_F(LabyrinthOfLore_Rendering_CameraTest, start_projection__with_a_valid_surf
    ASSERT_NE(nullptr, surface);
 
    camera.start_projection(surface);
+
+   al_destroy_bitmap(surface);
    SUCCEED();
+}
+
+
+TEST_F(LabyrinthOfLore_Rendering_CameraTest, start_projection__displaces_the_perspective_by_the_position)
+{
+   ALLEGRO_BITMAP *surface = al_get_backbuffer(al_get_current_display());
+
+   al_set_target_bitmap(surface);
+   al_clear_to_color(al_color_name("brown"));
+
+   LabyrinthOfLore::Rendering::Camera camera;
+   camera.start_projection(surface);
+
+   draw_targets();
+
+   //ASSERT_CENTRAL_PIXEL_MATCHES(surface, al_color_name("yellow"));
+
+   al_save_bitmap("tmp/save_test.png", surface);
+
+   //SUCCEED();
 }
 
 
