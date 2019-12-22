68669d8d4916d1723dea8d8fceaee9338d2af103
Mark Oates <markoates0@gmail.com>
2019-12-22 00:36:06 -0500

    Improve placement of vitality_and_mana_bar

--- src/LabyrinthOfLore/Rendering/Hud/Renderer.cpp
+++ src/LabyrinthOfLore/Rendering/Hud/Renderer.cpp
@@ -66,9 +66,9 @@ void Renderer::render_vitality_and_mana_bar()
 {
 ALLEGRO_FONT *font = font_bin->operator[]("gameovercre1.ttf -12");
 allegro_flare::placement3d placement{0, 0, 0};
-placement.size = AllegroFlare::vec3d(300, 100, 0);
+placement.size = AllegroFlare::vec3d(135, 20, 0);
 placement.scale = AllegroFlare::vec3d(2.0, 2.0, 2.0);
-placement.position = AllegroFlare::vec3d(al_get_bitmap_width(render_surface)/2, al_get_bitmap_height(render_surface) - 200 - 150, 0);
+placement.position = AllegroFlare::vec3d(al_get_bitmap_width(render_surface)/2, al_get_bitmap_height(render_surface) - 200 - 100, 0);
 
 LabyrinthOfLore::Rendering::Hud::VitalityAndManaBarRenderer vitality_and_mana_bar_renderer(font, vitality_and_mana_bar, placement);
 vitality_and_mana_bar_renderer.render();
