8b6bf478c837a6d98ad42dc6fa2d7409232c0abb
Mark Oates <markoates0@gmail.com>
2019-12-23 12:06:56 -0500

    Remove non-top vertexes on TileMapMeshCubeBuilder

--- src/LabyrinthOfLore/Rendering/TileMapMeshCubeBuilder.cpp
+++ src/LabyrinthOfLore/Rendering/TileMapMeshCubeBuilder.cpp
@@ -59,44 +59,6 @@ std::vector<ALLEGRO_VERTEX> result = {
   AllegroFlare::build_vertex(mul*x+1, y, height, cube_color, v, 0), // bottom right triangle
   AllegroFlare::build_vertex(mul*x, y+1, height, cube_color, 0, v),
   AllegroFlare::build_vertex(mul*x+1, y+1, height, cube_color, v, v),
-
-  // left
-  AllegroFlare::build_vertex(mul*x, y, height, cube_color, 0, 0), // top left triangle
-  AllegroFlare::build_vertex(mul*x, y+1, height, cube_color, v, 0),
-  AllegroFlare::build_vertex(mul*x, y, 0, cube_color, 0, v),
-
-  AllegroFlare::build_vertex(mul*x, y+1, height, cube_color, v, 0), // bottom right triangle
-  AllegroFlare::build_vertex(mul*x, y, 0, cube_color, 0, v),
-  AllegroFlare::build_vertex(mul*x, y+1, 0, cube_color, v, v),
-
-  // right
-  //AllegroFlare::build_vertex(mul*x+1, y+1, height, cube_color, 0, 0), // top left triangle
-  //AllegroFlare::build_vertex(mul*x+1, y, height, cube_color, v, 0),
-  //AllegroFlare::build_vertex(mul*x+1, y+1, 0, cube_color, 0, v),
-
-  //AllegroFlare::build_vertex(mul*x+1, y, height, cube_color, v, 0), // bottom right triangle
-  //AllegroFlare::build_vertex(mul*x+1, y+1, 0, cube_color, 0, v),
-  //AllegroFlare::build_vertex(mul*x+1, y, 0, cube_color, v, v),
-
-  // right
-  //AllegroFlare::build_vertex(mul*x+1, y+1, height, cube_color, 0, 0), // top left triangle
-  //AllegroFlare::build_vertex(mul*x+1, y, height, cube_color, v, 0),
-  //AllegroFlare::build_vertex(mul*x+1, y+1, 0, cube_color, 0, v),
-
-  //AllegroFlare::build_vertex(mul*x+1, y, height, cube_color, v, 0), // bottom right triangle
-  //AllegroFlare::build_vertex(mul*x+1, y+1, 0, cube_color, 0, v),
-  //AllegroFlare::build_vertex(mul*x+1, y, 0, cube_color, v, v),
-
-  // bottom
-  //AllegroFlare::build_vertex(mul*x, y+1, height, cube_color, 0, 0), // top left triangle
-  //AllegroFlare::build_vertex(mul*x+1, y+1, height, cube_color, v, 0),
-  //AllegroFlare::build_vertex(mul*x, y+1, 0, cube_color, 0, v),
-
-  //AllegroFlare::build_vertex(mul*x+1, y+1, height, cube_color, v, 0), // bottom right triangle
-  //AllegroFlare::build_vertex(mul*x, y+1, 0, cube_color, 0, v),
-  //AllegroFlare::build_vertex(mul*x+1, y+1, 0, cube_color, v, v),
-
-  //
 };
 
 for (auto &vertex : result)
