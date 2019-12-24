b913fea8a258d02541b478b8f5129eed929ec6d6
Mark Oates <markoates0@gmail.com>
2019-12-24 05:07:12 -0500

    Add some more comprehensive tests for Rendering/TileTypeDictionary

--- tests/LabyrinthOfLore/Rendering/TileTypeDictionaryTest.cpp
+++ tests/LabyrinthOfLore/Rendering/TileTypeDictionaryTest.cpp
@@ -33,3 +33,56 @@ TEST(LabyrinthOfLore_Rendering_TileTypeDictionaryTest, find_definition__returns_
    EXPECT_EQ_TILE_TYPE_DEFINITION(expected_definition, tile_type_dictionary.find_definition(0));
 }
 
+TEST(LabyrinthOfLore_Rendering_TileTypeDictionaryTest, find_definition__with_multiple_duplicate_entries__returns_the_first_definition_in_the_dictionary)
+{
+   LabyrinthOfLore::Rendering::TileTypeDictionary tile_type_dictionary({
+         { 0, LabyrinthOfLore::Rendering::TileTypeDefinition{ 3, 2, 1 } },
+         { 7, LabyrinthOfLore::Rendering::TileTypeDefinition{ 55, 14, 10 } },
+         { 2, LabyrinthOfLore::Rendering::TileTypeDefinition{ 7, 11, 82 } },
+         { 16, LabyrinthOfLore::Rendering::TileTypeDefinition{ 9, 2, 5 } },
+         { 7, LabyrinthOfLore::Rendering::TileTypeDefinition{ 73, 84, 91 } },
+      });
+
+   LabyrinthOfLore::Rendering::TileTypeDefinition expected_definition;
+
+   expected_definition = LabyrinthOfLore::Rendering::TileTypeDefinition(55, 14, 10);
+   EXPECT_EQ_TILE_TYPE_DEFINITION(expected_definition, tile_type_dictionary.find_definition(7));
+}
+
+TEST(LabyrinthOfLore_Rendering_TileTypeDictionaryTest, find_definition__returns_the_definition_in_the_dictionary_2)
+{
+   LabyrinthOfLore::Rendering::TileTypeDictionary tile_type_dictionary({
+         { 0, LabyrinthOfLore::Rendering::TileTypeDefinition{ 3, 2, 1 } },
+         { 7, LabyrinthOfLore::Rendering::TileTypeDefinition{ 55, 14, 10 } },
+         { 2, LabyrinthOfLore::Rendering::TileTypeDefinition{ 7, 11, 82 } },
+         { 16, LabyrinthOfLore::Rendering::TileTypeDefinition{ 9, 2, 5 } },
+         { 7, LabyrinthOfLore::Rendering::TileTypeDefinition{ 73, 84, 91 } },
+      });
+
+   LabyrinthOfLore::Rendering::TileTypeDefinition expected_definition;
+
+   expected_definition = LabyrinthOfLore::Rendering::TileTypeDefinition(3, 2, 1);
+   EXPECT_EQ_TILE_TYPE_DEFINITION(expected_definition, tile_type_dictionary.find_definition(0));
+
+   expected_definition = LabyrinthOfLore::Rendering::TileTypeDefinition(7, 11, 82);
+   EXPECT_EQ_TILE_TYPE_DEFINITION(expected_definition, tile_type_dictionary.find_definition(2));
+
+   expected_definition = LabyrinthOfLore::Rendering::TileTypeDefinition(9, 2, 5 );
+   EXPECT_EQ_TILE_TYPE_DEFINITION(expected_definition, tile_type_dictionary.find_definition(16));
+}
+
+TEST(LabyrinthOfLore_Rendering_TileTypeDictionaryTest, find_definition__can_use_definitions_with_netative_types__even_though_its_shouldnt_really_be_used)
+{
+   LabyrinthOfLore::Rendering::TileTypeDictionary tile_type_dictionary({
+         { -2, LabyrinthOfLore::Rendering::TileTypeDefinition{ 3, 2, 1 } },
+         { -999, LabyrinthOfLore::Rendering::TileTypeDefinition{ 55, 14, 10 } },
+      });
+
+   LabyrinthOfLore::Rendering::TileTypeDefinition expected_definition;
+
+   expected_definition = LabyrinthOfLore::Rendering::TileTypeDefinition(3, 2, 1);
+   EXPECT_EQ_TILE_TYPE_DEFINITION(expected_definition, tile_type_dictionary.find_definition(-2));
+
+   expected_definition = LabyrinthOfLore::Rendering::TileTypeDefinition(55, 14, 10);
+   EXPECT_EQ_TILE_TYPE_DEFINITION(expected_definition, tile_type_dictionary.find_definition(-999));
+}
