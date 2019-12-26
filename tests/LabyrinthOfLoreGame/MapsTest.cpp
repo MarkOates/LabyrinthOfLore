5ba50fcfa790cf2188b75998193723223efc91ac
Mark Oates <markoates0@gmail.com>
2019-12-26 13:04:11 -0500

    Use test fixtures in MapTest

--- tests/LabyrinthOfLoreGame/MapsTest.cpp
+++ tests/LabyrinthOfLoreGame/MapsTest.cpp
@@ -62,12 +62,12 @@ protected:
 
 #include <LabyrinthOfLoreGame/Maps.hpp>
 
-TEST(LabyrinthOfLoreGame_MapsTest, can_be_created_without_blowing_up)
+TEST_F(LabyrinthOfLoreGame_MapsTest, can_be_created_without_blowing_up)
 {
    LabyrinthOfLoreGame::Maps maps;
 }
 
-TEST(LabyrinthOfLoreGame_MapsTest, run__returns_the_expected_response)
+TEST_F(LabyrinthOfLoreGame_MapsTest, run__returns_the_expected_response)
 {
    LabyrinthOfLoreGame::Maps maps;
    std::vector<std::vector<LabyrinthOfLore::WorldMap::Tile>> actual_construct_tile_map_data = maps.build_construct_tile_map_data();
