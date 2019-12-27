5d08d88b414a83660cf596ef4e082e0ecd075273
Mark Oates <markoates0@gmail.com>
2019-12-26 21:23:23 -0500

    Add tests for descending elevation tiles

--- tests/LabyrinthOfLore/WorldMap/BitmapTileMapLoaderTest.cpp
+++ tests/LabyrinthOfLore/WorldMap/BitmapTileMapLoaderTest.cpp
@@ -277,14 +277,38 @@ TEST(LabyrinthOfLore_WorldMap_BitmapTileMapLoaderTest, load__correctly_picks_out
 
    ALLEGRO_BITMAP *source_bitmap = al_load_bitmap("/Users/markoates/Repos/LabyrinthOfLore/bin/programs/data/bitmaps/test_bitmap_tile_map_loader.png");
    ASSERT_NE(nullptr, source_bitmap);
-
-   LabyrinthOfLore::WorldMap::BitmapTileMapLoader loader(source_bitmap);
+LabyrinthOfLore::WorldMap::BitmapTileMapLoader loader(source_bitmap);
    LabyrinthOfLore::WorldMap::TileMap tile_map = loader.load();
 
    EXPECT_EQ(1, tile_map.get_tile(8, 8).get_type());
    EXPECT_EQ(6.0, tile_map.get_tile(8, 8).get_height());
-   EXPECT_EQ(1, tile_map.get_tile(16, 21).get_type());
-   EXPECT_EQ(6.0, tile_map.get_tile(16, 21).get_height());
+   EXPECT_EQ(1, tile_map.get_tile(16, 17).get_type());
+   EXPECT_EQ(6.0, tile_map.get_tile(16, 17).get_height());
+
+   al_uninstall_system();
+   SUCCEED();
+}
+
+TEST(LabyrinthOfLore_WorldMap_BitmapTileMapLoaderTest, load__correctly_picks_out_ascending_elevation_tiles)
+{
+   al_init();
+   al_init_image_addon();
+
+   ALLEGRO_BITMAP *source_bitmap = al_load_bitmap("/Users/markoates/Repos/LabyrinthOfLore/bin/programs/data/bitmaps/test_bitmap_tile_map_loader.png");
+   ASSERT_NE(nullptr, source_bitmap);
+
+   LabyrinthOfLore::WorldMap::BitmapTileMapLoader loader(source_bitmap);
+   LabyrinthOfLore::WorldMap::TileMap tile_map = loader.load();
+
+   EXPECT_EQ(1, tile_map.get_tile(14, 19).get_type());
+   EXPECT_EQ(6.25, tile_map.get_tile(14, 19).get_height());
+   EXPECT_EQ(1, tile_map.get_tile(15, 21).get_type());
+   EXPECT_EQ(6.5, tile_map.get_tile(15, 21).get_height());
+   EXPECT_EQ(1, tile_map.get_tile(15, 24).get_type());
+   EXPECT_EQ(6.75, tile_map.get_tile(15, 24).get_height());
+   EXPECT_EQ(1, tile_map.get_tile(15, 25).get_type());
+   EXPECT_EQ(7.0, tile_map.get_tile(15, 25).get_height());
+
 
    al_uninstall_system();
    SUCCEED();
