1106cfb00278f6b7dec91ed3539c05776403288a
Mark Oates <markoates0@gmail.com>
2019-12-28 12:28:46 -0500

    Revert "Pass in vec3d as additional parameter in stored observed state for Physics/EntityZoneCollisionObserver"
    This reverts commit c37b740f1a8c1b7dcca9a13d41f7678954a96fe9.

--- src/LabyrinthOfLore/Physics/EntityZoneCollisionObserver.cpp
+++ src/LabyrinthOfLore/Physics/EntityZoneCollisionObserver.cpp
@@ -22,27 +22,27 @@ EntityZoneCollisionObserver::~EntityZoneCollisionObserver()
 }
 
 
-std::vector<std::tuple<LabyrinthOfLore::Entity::Base*, AllegroFlare::vec3d, LabyrinthOfLore::WorldMap::Zone*>> EntityZoneCollisionObserver::get_collisions_at_start()
+std::vector<std::tuple<LabyrinthOfLore::Entity::Base*, LabyrinthOfLore::WorldMap::Zone*>> EntityZoneCollisionObserver::get_collisions_at_start()
 {
    return collisions_at_start;
 }
 
 
-std::vector<std::tuple<LabyrinthOfLore::Entity::Base*, AllegroFlare::vec3d, LabyrinthOfLore::WorldMap::Zone*>> EntityZoneCollisionObserver::get_collisions_at_end()
+std::vector<std::tuple<LabyrinthOfLore::Entity::Base*, LabyrinthOfLore::WorldMap::Zone*>> EntityZoneCollisionObserver::get_collisions_at_end()
 {
    return collisions_at_end;
 }
 
 
-std::vector<std::tuple<LabyrinthOfLore::Entity::Base*, AllegroFlare::vec3d, LabyrinthOfLore::WorldMap::Zone*>> EntityZoneCollisionObserver::get_collided(std::vector<LabyrinthOfLore::Entity::Base*> entities, std::vector<LabyrinthOfLore::WorldMap::Zone*> zones)
+std::vector<std::tuple<LabyrinthOfLore::Entity::Base*, LabyrinthOfLore::WorldMap::Zone*>> EntityZoneCollisionObserver::get_collided(std::vector<LabyrinthOfLore::Entity::Base*> entities, std::vector<LabyrinthOfLore::WorldMap::Zone*> zones)
 {
-std::vector<std::tuple<LabyrinthOfLore::Entity::Base*, AllegroFlare::vec3d, LabyrinthOfLore::WorldMap::Zone*>> result;
+std::vector<std::tuple<LabyrinthOfLore::Entity::Base*, LabyrinthOfLore::WorldMap::Zone*>> result;
 
 for (auto &entity : entities)
 {
    for (auto &zone : zones)
    {
-      if (zone->collides(entity->get_placement_ref().position)) result.push_back({ entity, {}, zone });
+      if (zone->collides(entity->get_placement_ref().position)) result.push_back({ entity, zone });
    }
 }
 
