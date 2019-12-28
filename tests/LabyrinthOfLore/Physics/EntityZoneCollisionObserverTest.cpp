c438dd322c66bd9ea264225c961f6913f7161ae1
Mark Oates <markoates0@gmail.com>
2019-12-28 11:01:01 -0500

    Fix not-actually-asserted test; Add "does" and "does not" tests for Physics/EntityZoneCollisionObserver

--- tests/LabyrinthOfLore/Physics/EntityZoneCollisionObserverTest.cpp
+++ tests/LabyrinthOfLore/Physics/EntityZoneCollisionObserverTest.cpp
@@ -25,11 +25,33 @@ TEST(LabyrinthOfLore_Physics_EntityZoneCollisionObserverTest, get_collided__retu
    };
 
    LabyrinthOfLore::Physics::EntityZoneCollisionObserver entity_zone_collision_observer(entities, zones);
-   entity_zone_collision_observer.get_collided();
 
    std::vector<std::pair<LabyrinthOfLore::Entity::Base*, LabyrinthOfLore::WorldMap::Zone*>> expected_collided = {
       { entities[0], zones[0] },
    };
+   std::vector<std::pair<LabyrinthOfLore::Entity::Base*, LabyrinthOfLore::WorldMap::Zone*>> actual_collided = entity_zone_collision_observer.get_collided();
+
+   ASSERT_EQ(expected_collided, actual_collided);
+
+   for (auto &entity : entities) delete entity;
+   for (auto &zone : zones) delete zone;
+}
+
+TEST(LabyrinthOfLore_Physics_EntityZoneCollisionObserverTest, get_collided__does_not_return_entities_and_zones_that_are_not_colliding)
+{
+   std::vector<LabyrinthOfLore::Entity::Base*> entities = {
+      new LabyrinthOfLore::Entity::Base(nullptr, nullptr, { 1.5, -1.5, 0.5 })
+   };
+   std::vector<LabyrinthOfLore::WorldMap::Zone*> zones = {
+      new LabyrinthOfLore::WorldMap::Zone(0, 0, 0, 1, 1, 1)
+   };
+
+   LabyrinthOfLore::Physics::EntityZoneCollisionObserver entity_zone_collision_observer(entities, zones);
+
+   std::vector<std::pair<LabyrinthOfLore::Entity::Base*, LabyrinthOfLore::WorldMap::Zone*>> expected_collided = {};
+   std::vector<std::pair<LabyrinthOfLore::Entity::Base*, LabyrinthOfLore::WorldMap::Zone*>> actual_collided = entity_zone_collision_observer.get_collided();
+
+   ASSERT_EQ(expected_collided, actual_collided);
 
    for (auto &entity : entities) delete entity;
    for (auto &zone : zones) delete zone;
