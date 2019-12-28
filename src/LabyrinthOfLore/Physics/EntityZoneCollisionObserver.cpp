11867c37f281f68218f7d6a4dd13949bb4e5bd56
Mark Oates <markoates0@gmail.com>
2019-12-28 12:02:23 -0500

    Remove function not yet implemented

--- src/LabyrinthOfLore/Physics/EntityZoneCollisionObserver.cpp
+++ src/LabyrinthOfLore/Physics/EntityZoneCollisionObserver.cpp
@@ -66,12 +66,6 @@ collisions_at_end = get_collided(entities, zones);
 return;
 
 }
-
-std::vector<std::pair<LabyrinthOfLore::Entity::Base*, LabyrinthOfLore::WorldMap::Zone*>> EntityZoneCollisionObserver::collisions_added()
-{
-return {};
-
-}
 } // namespace Physics
 } // namespace LabyrinthOfLore
 
