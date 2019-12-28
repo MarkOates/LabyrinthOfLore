3305433fd3d5d24bddf45ba835d133d4cf104d0b
Mark Oates <markoates0@gmail.com>
2019-12-27 20:17:45 -0500

    Move some comments/blank spaces to improve clarity

--- src/LabyrinthOfLore/Rendering/TileMapMeshPillarTexturer.cpp
+++ src/LabyrinthOfLore/Rendering/TileMapMeshPillarTexturer.cpp
@@ -59,8 +59,8 @@ float v2 = 0;
 int this_face_start_index = 0;
 int num_vertexes_in_face = 6;
 
-tile_atlas->get_tile_uv(tile_index_for_top_texture, &u1, &v1, &u2, &v2);
 // top face
+tile_atlas->get_tile_uv(tile_index_for_top_texture, &u1, &v1, &u2, &v2);
 pillar_vertexes[this_face_start_index+0].u = u1;
 pillar_vertexes[this_face_start_index+0].v = v2;//1.0 * 128;
 pillar_vertexes[this_face_start_index+1].u = u2;//1.0 * 128;
@@ -73,70 +73,14 @@ pillar_vertexes[this_face_start_index+4].u = u1;
 pillar_vertexes[this_face_start_index+4].v = v2;//1.0 * 128;
 pillar_vertexes[this_face_start_index+5].u = u2;//1.0 * 128;
 pillar_vertexes[this_face_start_index+5].v = v2;//1.0 * 128;
-
 this_face_start_index += num_vertexes_in_face;
 
 
 if (infer_number_of_front_right_left_back_sets_of_vertexes() > 0)
 {
-   tile_atlas->get_tile_uv(tile_index_for_front_and_back_texture, &u1, &v1, &u2, &v2);
-   if (needs_partial_height_side_faces_from_top) { v1 = (v1 - v2) * fmod(height, 1.0) + v2; }
-
    // front face
-   pillar_vertexes[this_face_start_index+0].u = u1;
-   pillar_vertexes[this_face_start_index+0].v = v2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+1].u = u2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+1].v = v1;
-   pillar_vertexes[this_face_start_index+2].u = u1;
-   pillar_vertexes[this_face_start_index+2].v = v1;
-   pillar_vertexes[this_face_start_index+3].u = u2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+3].v = v1;
-   pillar_vertexes[this_face_start_index+4].u = u1;
-   pillar_vertexes[this_face_start_index+4].v = v2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+5].u = u2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+5].v = v2;//1.0 * 128;
-
-   this_face_start_index += num_vertexes_in_face;
-
-   tile_atlas->get_tile_uv(tile_index_for_right_and_left_texture, &u1, &v1, &u2, &v2);
-   if (needs_partial_height_side_faces_from_top) { v1 = (v1 - v2) * fmod(height, 1.0) + v2; }
-   // right face
-   pillar_vertexes[this_face_start_index+0].u = u1;
-   pillar_vertexes[this_face_start_index+0].v = v2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+1].u = u2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+1].v = v1;
-   pillar_vertexes[this_face_start_index+2].u = u1;
-   pillar_vertexes[this_face_start_index+2].v = v1;
-   pillar_vertexes[this_face_start_index+3].u = u2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+3].v = v1;
-   pillar_vertexes[this_face_start_index+4].u = u1;
-   pillar_vertexes[this_face_start_index+4].v = v2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+5].u = u2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+5].v = v2;//1.0 * 128;
-
-   this_face_start_index += num_vertexes_in_face;
-
-   tile_atlas->get_tile_uv(tile_index_for_right_and_left_texture, &u1, &v1, &u2, &v2);
-   if (needs_partial_height_side_faces_from_top) { v1 = (v1 - v2) * fmod(height, 1.0) + v2; }
-   // left face
-   pillar_vertexes[this_face_start_index+0].u = u1;
-   pillar_vertexes[this_face_start_index+0].v = v2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+1].u = u2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+1].v = v1;
-   pillar_vertexes[this_face_start_index+2].u = u1;
-   pillar_vertexes[this_face_start_index+2].v = v1;
-   pillar_vertexes[this_face_start_index+3].u = u2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+3].v = v1;
-   pillar_vertexes[this_face_start_index+4].u = u1;
-   pillar_vertexes[this_face_start_index+4].v = v2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+5].u = u2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+5].v = v2;//1.0 * 128;
-
-   this_face_start_index += num_vertexes_in_face;
-
    tile_atlas->get_tile_uv(tile_index_for_front_and_back_texture, &u1, &v1, &u2, &v2);
    if (needs_partial_height_side_faces_from_top) { v1 = (v1 - v2) * fmod(height, 1.0) + v2; }
-   // back face
    pillar_vertexes[this_face_start_index+0].u = u1;
    pillar_vertexes[this_face_start_index+0].v = v2;//1.0 * 128;
    pillar_vertexes[this_face_start_index+1].u = u2;//1.0 * 128;
@@ -149,65 +93,64 @@ if (infer_number_of_front_right_left_back_sets_of_vertexes() > 0)
    pillar_vertexes[this_face_start_index+4].v = v2;//1.0 * 128;
    pillar_vertexes[this_face_start_index+5].u = u2;//1.0 * 128;
    pillar_vertexes[this_face_start_index+5].v = v2;//1.0 * 128;
+   this_face_start_index += num_vertexes_in_face;
 
+   // right face
+   tile_atlas->get_tile_uv(tile_index_for_right_and_left_texture, &u1, &v1, &u2, &v2);
+   if (needs_partial_height_side_faces_from_top) { v1 = (v1 - v2) * fmod(height, 1.0) + v2; }
+   pillar_vertexes[this_face_start_index+0].u = u1;
+   pillar_vertexes[this_face_start_index+0].v = v2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+1].u = u2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+1].v = v1;
+   pillar_vertexes[this_face_start_index+2].u = u1;
+   pillar_vertexes[this_face_start_index+2].v = v1;
+   pillar_vertexes[this_face_start_index+3].u = u2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+3].v = v1;
+   pillar_vertexes[this_face_start_index+4].u = u1;
+   pillar_vertexes[this_face_start_index+4].v = v2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+5].u = u2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+5].v = v2;//1.0 * 128;
+   this_face_start_index += num_vertexes_in_face;
+
+   // left face
+   tile_atlas->get_tile_uv(tile_index_for_right_and_left_texture, &u1, &v1, &u2, &v2);
+   if (needs_partial_height_side_faces_from_top) { v1 = (v1 - v2) * fmod(height, 1.0) + v2; }
+   pillar_vertexes[this_face_start_index+0].u = u1;
+   pillar_vertexes[this_face_start_index+0].v = v2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+1].u = u2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+1].v = v1;
+   pillar_vertexes[this_face_start_index+2].u = u1;
+   pillar_vertexes[this_face_start_index+2].v = v1;
+   pillar_vertexes[this_face_start_index+3].u = u2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+3].v = v1;
+   pillar_vertexes[this_face_start_index+4].u = u1;
+   pillar_vertexes[this_face_start_index+4].v = v2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+5].u = u2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+5].v = v2;//1.0 * 128;
+   this_face_start_index += num_vertexes_in_face;
+
+   // back face
+   tile_atlas->get_tile_uv(tile_index_for_front_and_back_texture, &u1, &v1, &u2, &v2);
+   if (needs_partial_height_side_faces_from_top) { v1 = (v1 - v2) * fmod(height, 1.0) + v2; }
+   pillar_vertexes[this_face_start_index+0].u = u1;
+   pillar_vertexes[this_face_start_index+0].v = v2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+1].u = u2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+1].v = v1;
+   pillar_vertexes[this_face_start_index+2].u = u1;
+   pillar_vertexes[this_face_start_index+2].v = v1;
+   pillar_vertexes[this_face_start_index+3].u = u2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+3].v = v1;
+   pillar_vertexes[this_face_start_index+4].u = u1;
+   pillar_vertexes[this_face_start_index+4].v = v2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+5].u = u2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+5].v = v2;//1.0 * 128;
    this_face_start_index += num_vertexes_in_face;
 }
 
 for (int pass_num=1; pass_num<infer_number_of_front_right_left_back_sets_of_vertexes(); pass_num++)
 {
-   tile_atlas->get_tile_uv(tile_index_for_front_and_back_texture, &u1, &v1, &u2, &v2);
    // front face
-   pillar_vertexes[this_face_start_index+0].u = u1;
-   pillar_vertexes[this_face_start_index+0].v = v2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+1].u = u2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+1].v = v1;
-   pillar_vertexes[this_face_start_index+2].u = u1;
-   pillar_vertexes[this_face_start_index+2].v = v1;
-   pillar_vertexes[this_face_start_index+3].u = u2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+3].v = v1;
-   pillar_vertexes[this_face_start_index+4].u = u1;
-   pillar_vertexes[this_face_start_index+4].v = v2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+5].u = u2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+5].v = v2;//1.0 * 128;
-
-   this_face_start_index += num_vertexes_in_face;
-
-   tile_atlas->get_tile_uv(tile_index_for_right_and_left_texture, &u1, &v1, &u2, &v2);
-   // right face
-   pillar_vertexes[this_face_start_index+0].u = u1;
-   pillar_vertexes[this_face_start_index+0].v = v2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+1].u = u2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+1].v = v1;
-   pillar_vertexes[this_face_start_index+2].u = u1;
-   pillar_vertexes[this_face_start_index+2].v = v1;
-   pillar_vertexes[this_face_start_index+3].u = u2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+3].v = v1;
-   pillar_vertexes[this_face_start_index+4].u = u1;
-   pillar_vertexes[this_face_start_index+4].v = v2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+5].u = u2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+5].v = v2;//1.0 * 128;
-
-   this_face_start_index += num_vertexes_in_face;
-
-   tile_atlas->get_tile_uv(tile_index_for_right_and_left_texture, &u1, &v1, &u2, &v2);
-   // left face
-   pillar_vertexes[this_face_start_index+0].u = u1;
-   pillar_vertexes[this_face_start_index+0].v = v2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+1].u = u2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+1].v = v1;
-   pillar_vertexes[this_face_start_index+2].u = u1;
-   pillar_vertexes[this_face_start_index+2].v = v1;
-   pillar_vertexes[this_face_start_index+3].u = u2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+3].v = v1;
-   pillar_vertexes[this_face_start_index+4].u = u1;
-   pillar_vertexes[this_face_start_index+4].v = v2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+5].u = u2;//1.0 * 128;
-   pillar_vertexes[this_face_start_index+5].v = v2;//1.0 * 128;
-
-   this_face_start_index += num_vertexes_in_face;
-
    tile_atlas->get_tile_uv(tile_index_for_front_and_back_texture, &u1, &v1, &u2, &v2);
-   // back face
    pillar_vertexes[this_face_start_index+0].u = u1;
    pillar_vertexes[this_face_start_index+0].v = v2;//1.0 * 128;
    pillar_vertexes[this_face_start_index+1].u = u2;//1.0 * 128;
@@ -220,7 +163,54 @@ for (int pass_num=1; pass_num<infer_number_of_front_right_left_back_sets_of_vert
    pillar_vertexes[this_face_start_index+4].v = v2;//1.0 * 128;
    pillar_vertexes[this_face_start_index+5].u = u2;//1.0 * 128;
    pillar_vertexes[this_face_start_index+5].v = v2;//1.0 * 128;
+   this_face_start_index += num_vertexes_in_face;
 
+   // right face
+   tile_atlas->get_tile_uv(tile_index_for_right_and_left_texture, &u1, &v1, &u2, &v2);
+   pillar_vertexes[this_face_start_index+0].u = u1;
+   pillar_vertexes[this_face_start_index+0].v = v2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+1].u = u2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+1].v = v1;
+   pillar_vertexes[this_face_start_index+2].u = u1;
+   pillar_vertexes[this_face_start_index+2].v = v1;
+   pillar_vertexes[this_face_start_index+3].u = u2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+3].v = v1;
+   pillar_vertexes[this_face_start_index+4].u = u1;
+   pillar_vertexes[this_face_start_index+4].v = v2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+5].u = u2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+5].v = v2;//1.0 * 128;
+   this_face_start_index += num_vertexes_in_face;
+
+   // left face
+   tile_atlas->get_tile_uv(tile_index_for_right_and_left_texture, &u1, &v1, &u2, &v2);
+   pillar_vertexes[this_face_start_index+0].u = u1;
+   pillar_vertexes[this_face_start_index+0].v = v2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+1].u = u2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+1].v = v1;
+   pillar_vertexes[this_face_start_index+2].u = u1;
+   pillar_vertexes[this_face_start_index+2].v = v1;
+   pillar_vertexes[this_face_start_index+3].u = u2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+3].v = v1;
+   pillar_vertexes[this_face_start_index+4].u = u1;
+   pillar_vertexes[this_face_start_index+4].v = v2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+5].u = u2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+5].v = v2;//1.0 * 128;
+   this_face_start_index += num_vertexes_in_face;
+
+   // back face
+   tile_atlas->get_tile_uv(tile_index_for_front_and_back_texture, &u1, &v1, &u2, &v2);
+   pillar_vertexes[this_face_start_index+0].u = u1;
+   pillar_vertexes[this_face_start_index+0].v = v2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+1].u = u2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+1].v = v1;
+   pillar_vertexes[this_face_start_index+2].u = u1;
+   pillar_vertexes[this_face_start_index+2].v = v1;
+   pillar_vertexes[this_face_start_index+3].u = u2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+3].v = v1;
+   pillar_vertexes[this_face_start_index+4].u = u1;
+   pillar_vertexes[this_face_start_index+4].v = v2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+5].u = u2;//1.0 * 128;
+   pillar_vertexes[this_face_start_index+5].v = v2;//1.0 * 128;
    this_face_start_index += num_vertexes_in_face;
 }
 
