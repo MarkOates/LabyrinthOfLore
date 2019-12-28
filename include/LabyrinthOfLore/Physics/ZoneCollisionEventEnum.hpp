f7ec4fa5a63d5f21c9cda62fc89ae6df83fd84a0
Mark Oates <markoates0@gmail.com>
2019-12-28 10:14:45 -0500

    Add Physics/ZoneCollisionEventEnum

---
+++ include/LabyrinthOfLore/Physics/ZoneCollisionEventEnum.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace LabyrinthOfLore
+{
+   namespace Physics
+   {
+      enum zone_collision_event_t
+      {
+         COLLISION_EVENT_NONE=0,
+         COLLISION_EVENT_ENTERED,
+         COLLISION_EVENT_EXITED,
+      };
+   } // namespace Physics
+} // namespace LabyrinthOfLore
+
