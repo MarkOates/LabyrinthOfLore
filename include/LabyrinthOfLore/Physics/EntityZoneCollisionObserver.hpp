c4acb88487f287c975d862e03cbeb461a3d01592
Mark Oates <markoates0@gmail.com>
2019-12-28 11:50:54 -0500

    Replace some usage of "vector" with "tuple"

--- include/LabyrinthOfLore/Physics/EntityZoneCollisionObserver.hpp
+++ include/LabyrinthOfLore/Physics/EntityZoneCollisionObserver.hpp
@@ -3,6 +3,7 @@
 
 #include <LabyrinthOfLore/Entity/Base.hpp>
 #include <LabyrinthOfLore/WorldMap/Zone.hpp>
+#include <tuple>
 #include <utility>
 #include <vector>
 
@@ -14,17 +15,17 @@ namespace LabyrinthOfLore
       class EntityZoneCollisionObserver
       {
       private:
-         std::vector<std::pair<LabyrinthOfLore::Entity::Base*, LabyrinthOfLore::WorldMap::Zone*>> collisions_at_start;
-         std::vector<std::pair<LabyrinthOfLore::Entity::Base*, LabyrinthOfLore::WorldMap::Zone*>> collisions_at_end;
+         std::vector<std::tuple<LabyrinthOfLore::Entity::Base*, LabyrinthOfLore::WorldMap::Zone*>> collisions_at_start;
+         std::vector<std::tuple<LabyrinthOfLore::Entity::Base*, LabyrinthOfLore::WorldMap::Zone*>> collisions_at_end;
 
       public:
          EntityZoneCollisionObserver();
          ~EntityZoneCollisionObserver();
 
 
-         std::vector<std::pair<LabyrinthOfLore::Entity::Base*, LabyrinthOfLore::WorldMap::Zone*>> get_collisions_at_start();
-         std::vector<std::pair<LabyrinthOfLore::Entity::Base*, LabyrinthOfLore::WorldMap::Zone*>> get_collisions_at_end();
-      std::vector<std::pair<LabyrinthOfLore::Entity::Base*, LabyrinthOfLore::WorldMap::Zone*>> get_collided(std::vector<LabyrinthOfLore::Entity::Base*> entities={}, std::vector<LabyrinthOfLore::WorldMap::Zone*> zones={});
+         std::vector<std::tuple<LabyrinthOfLore::Entity::Base*, LabyrinthOfLore::WorldMap::Zone*>> get_collisions_at_start();
+         std::vector<std::tuple<LabyrinthOfLore::Entity::Base*, LabyrinthOfLore::WorldMap::Zone*>> get_collisions_at_end();
+      std::vector<std::tuple<LabyrinthOfLore::Entity::Base*, LabyrinthOfLore::WorldMap::Zone*>> get_collided(std::vector<LabyrinthOfLore::Entity::Base*> entities={}, std::vector<LabyrinthOfLore::WorldMap::Zone*> zones={});
       void observe_start_state(std::vector<LabyrinthOfLore::Entity::Base*> entities={}, std::vector<LabyrinthOfLore::WorldMap::Zone*> zones={});
       void observe_end_state(std::vector<LabyrinthOfLore::Entity::Base*> entities={}, std::vector<LabyrinthOfLore::WorldMap::Zone*> zones={});
       std::vector<std::pair<LabyrinthOfLore::Entity::Base*, LabyrinthOfLore::WorldMap::Zone*>> collisions_added();
