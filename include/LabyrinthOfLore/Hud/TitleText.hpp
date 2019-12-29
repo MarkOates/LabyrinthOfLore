ecf3bf51b52234b2dd3c925bda7cc4e17820fec1
Mark Oates <markoates0@gmail.com>
2019-12-28 19:00:32 -0500

    Add level title text (might need to disable as working on it)

--- include/LabyrinthOfLore/Hud/TitleText.hpp
+++ include/LabyrinthOfLore/Hud/TitleText.hpp
@@ -11,13 +11,18 @@ namespace LabyrinthOfLore
       class TitleText
       {
       private:
+         std::string above_text;
+         std::string headline_text;
 
       public:
          TitleText();
          ~TitleText();
 
+         void set_above_text(std::string above_text);
+         void set_headline_text(std::string headline_text);
 
-      std::string run();
+         std::string get_above_text();
+         std::string get_headline_text();
       };
    }
 }
--- src/LabyrinthOfLore/Hud/TitleText.cpp
+++ src/LabyrinthOfLore/Hud/TitleText.cpp
@@ -11,6 +11,8 @@ namespace Hud
 
 
 TitleText::TitleText()
+   : above_text("Level: N")
+   , headline_text("The Undefined Level")
 {
 }
 
@@ -20,10 +22,30 @@ TitleText::~TitleText()
 }
 
 
-std::string TitleText::run()
+void TitleText::set_above_text(std::string above_text)
 {
-return "Hello World!";
+   this->above_text = above_text;
 }
+
+
+void TitleText::set_headline_text(std::string headline_text)
+{
+   this->headline_text = headline_text;
+}
+
+
+std::string TitleText::get_above_text()
+{
+   return above_text;
+}
+
+
+std::string TitleText::get_headline_text()
+{
+   return headline_text;
+}
+
+
 } // namespace Hud
 } // namespace LabyrinthOfLore
 
