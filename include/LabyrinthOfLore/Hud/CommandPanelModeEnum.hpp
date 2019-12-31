62904832ed3ceee15830cfba5e34615b822dad73
Mark Oates <markoates0@gmail.com>
2019-12-30 19:58:22 -0500

    Add LabyrinthOfLore/Hud/CommandPanelModeEnum

---
+++ include/LabyrinthOfLore/Hud/CommandPanelModeEnum.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace LabyrinthOfLore
+{
+   namespace Hud
+   {
+      enum command_mode_t
+      {
+         COMMAND_MODE_NONE = 0,
+         COMMAND_MODE_TALK,
+         COMMAND_MODE_PICKUP,
+         COMMAND_MODE_LOOK,
+         COMMAND_MODE_ATTACK,
+         COMMAND_MODE_USE,
+      };
+   } // namespace Hud
+} // namespace LabyrinthOfLore
+
+
