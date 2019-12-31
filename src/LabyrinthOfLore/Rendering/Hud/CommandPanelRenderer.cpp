b9074b413609feaf5f950bc87f3fdc02c1978dbb
Mark Oates <markoates0@gmail.com>
2019-12-30 20:12:48 -0500

    Render different command modes with CommandPanelRenderer

--- src/LabyrinthOfLore/Rendering/Hud/CommandPanelRenderer.cpp
+++ src/LabyrinthOfLore/Rendering/Hud/CommandPanelRenderer.cpp
@@ -40,19 +40,25 @@ place.start_transform();
 //{
 //}
 
-std::vector<LabyrinthOfLore::Hud::command_mode_t> modes_for_display = {
-   LabyrinthOfLore::Hud::COMMAND_MODE_NONE,
-   LabyrinthOfLore::Hud::COMMAND_MODE_TALK,
-   LabyrinthOfLore::Hud::COMMAND_MODE_PICKUP,
-   LabyrinthOfLore::Hud::COMMAND_MODE_LOOK,
-   LabyrinthOfLore::Hud::COMMAND_MODE_ATTACK,
-   LabyrinthOfLore::Hud::COMMAND_MODE_USE,
+std::vector<std::pair<std::string, LabyrinthOfLore::Hud::command_mode_t>> modes_for_display = {
+   { "[T]alk", LabyrinthOfLore::Hud::COMMAND_MODE_TALK, },
+   { "[P]ickup", LabyrinthOfLore::Hud::COMMAND_MODE_PICKUP, },
+   { "[L]look", LabyrinthOfLore::Hud::COMMAND_MODE_LOOK, },
+   { "Attac[k]", LabyrinthOfLore::Hud::COMMAND_MODE_ATTACK, },
+   { "[U]se", LabyrinthOfLore::Hud::COMMAND_MODE_USE, },
 };
 
+float top_bottom_padding = 30.0f;
+float center_y = place.size.y/2;
+float full_height = (place.size.y - top_bottom_padding);
+float vertical_element_spacing = full_height / modes_for_display.size();
 
-
-
-al_draw_text(font, al_color_name("white"), 0, 0, 0, " - command panel - ");
+int i = 0;
+for (auto &mode_for_display : modes_for_display)
+{
+   i++;
+   al_draw_text(font, al_color_name("white"), place.size.x/2, vertical_element_spacing * i, ALLEGRO_ALIGN_CENTRE, mode_for_display.first.c_str());
+}
 
 place.restore_transform();
 return;
