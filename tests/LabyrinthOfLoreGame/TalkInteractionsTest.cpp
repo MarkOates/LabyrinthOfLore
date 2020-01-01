e59ff86bca9c3d0ce30c50ef50bf3f012892b601
Mark Oates <markoates0@gmail.com>
2020-01-01 02:14:38 -0500

    Fix some spacing

--- tests/LabyrinthOfLoreGame/TalkInteractionsTest.cpp
+++ tests/LabyrinthOfLoreGame/TalkInteractionsTest.cpp
@@ -61,37 +61,37 @@ TEST_F(LabyrinthOfLoreGame_TalkInteractionsTest, can_be_created_without_blowing_
 
 TEST_F(LabyrinthOfLoreGame_TalkInteractionsTest, validate_arguments__with_default_values__raises_an_exception_requiring_an_entity_with_a_thing_id)
 {
-    std::vector<LabyrinthOfLore::Entity::Base*> all_entities;
-    LabyrinthOfLore::Entity::Base entity;
-    LabyrinthOfLore::Entity::ThingDictionary thing_dictionary;
-    LabyrinthOfLore::Hud::MessageScroll message_scroll;
-    LabyrinthOfLore::Hud::CharacterPanel character_panel;
-    AllegroFlare::Inventory player_inventory;
+   std::vector<LabyrinthOfLore::Entity::Base*> all_entities;
+   LabyrinthOfLore::Entity::Base entity;
+   LabyrinthOfLore::Entity::ThingDictionary thing_dictionary;
+   LabyrinthOfLore::Hud::MessageScroll message_scroll;
+   LabyrinthOfLore::Hud::CharacterPanel character_panel;
+   AllegroFlare::Inventory player_inventory;
 
-    LabyrinthOfLoreGame::TalkInteractions talk_interactions(&all_entities, &entity, &thing_dictionary, &message_scroll, &character_panel, &player_inventory);
-    std::string expected_error_message = "Cannot validate_arguments expecting the entity to have a \"thing_id\" but it does not.";
-    ASSERT_THROW_WITH_MESSAGE(talk_interactions.validate_arguments(), std::runtime_error, expected_error_message);
+   LabyrinthOfLoreGame::TalkInteractions talk_interactions(&all_entities, &entity, &thing_dictionary, &message_scroll, &character_panel, &player_inventory);
+   std::string expected_error_message = "Cannot validate_arguments expecting the entity to have a \"thing_id\" but it does not.";
+   ASSERT_THROW_WITH_MESSAGE(talk_interactions.validate_arguments(), std::runtime_error, expected_error_message);
 }
 
 TEST_F(LabyrinthOfLoreGame_TalkInteractionsTest, validate_arguments__works_with_safe_values)
 {
-    std::vector<LabyrinthOfLore::Entity::Base*> all_entities;
-    LabyrinthOfLore::Entity::Base entity;
-    entity.set("thing_id", 1);
-    LabyrinthOfLore::Entity::ThingDictionary thing_dictionary;
-    LabyrinthOfLore::Hud::MessageScroll message_scroll;
-    LabyrinthOfLore::Hud::CharacterPanel character_panel;
-    AllegroFlare::Inventory player_inventory;
+   std::vector<LabyrinthOfLore::Entity::Base*> all_entities;
+   LabyrinthOfLore::Entity::Base entity;
+   entity.set("thing_id", 1);
+   LabyrinthOfLore::Entity::ThingDictionary thing_dictionary;
+   LabyrinthOfLore::Hud::MessageScroll message_scroll;
+   LabyrinthOfLore::Hud::CharacterPanel character_panel;
+   AllegroFlare::Inventory player_inventory;
 
-    LabyrinthOfLoreGame::TalkInteractions talk_interactions(&all_entities, &entity, &thing_dictionary, &message_scroll, &character_panel, &player_inventory);
-    talk_interactions.validate_arguments();
-    SUCCEED();
+   LabyrinthOfLoreGame::TalkInteractions talk_interactions(&all_entities, &entity, &thing_dictionary, &message_scroll, &character_panel, &player_inventory);
+   talk_interactions.validate_arguments();
+   SUCCEED();
 }
 
 TEST_F(LabyrinthOfLoreGame_TalkInteractionsTest, works_with_the_fixture)
 {
-    LabyrinthOfLoreGame::TalkInteractions talk_interactions(&all_entities, &entity, &thing_dictionary, &message_scroll, &character_panel, &player_inventory);
-    talk_interactions.validate_arguments();
-    SUCCEED();
+   LabyrinthOfLoreGame::TalkInteractions talk_interactions(&all_entities, &entity, &thing_dictionary, &message_scroll, &character_panel, &player_inventory);
+   talk_interactions.validate_arguments();
+   SUCCEED();
 }
 
