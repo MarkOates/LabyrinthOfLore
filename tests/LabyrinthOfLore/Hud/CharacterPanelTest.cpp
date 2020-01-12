97a6002fc896ed0db5b048842b9b9b12f7684e6b
Mark Oates <markoates0@gmail.com>
2020-01-12 00:44:39 -0500

    Add "ASSERT_THROW_WITH_MESSAGE" to Hud/CharacterPanel test file

--- tests/LabyrinthOfLore/Hud/CharacterPanelTest.cpp
+++ tests/LabyrinthOfLore/Hud/CharacterPanelTest.cpp
@@ -1,6 +1,11 @@
 
 #include <gtest/gtest.h>
 
+#define ASSERT_THROW_WITH_MESSAGE(code, raised_exception_type, raised_exception_message) \
+   try { code; FAIL() << "Expected " # raised_exception_type; } \
+   catch ( raised_exception_type const &err ) { EXPECT_EQ(err.what(), std::string( raised_exception_message )); } \
+   catch (...) { FAIL() << "Expected " # raised_exception_type; }
+
 #include <LabyrinthOfLore/Hud/CharacterPanel.hpp>
 #include <LabyrinthOfLoreGame/ItemIdEnums.hpp>
 
