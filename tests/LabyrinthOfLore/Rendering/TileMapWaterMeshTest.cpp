1c950e5b4e5a71d394d09c0ab8aba7cd8381dfad
Mark Oates <mark.oates@shopify.com>
2020-01-14 11:46:46 -0500

    Use "3x3_test_texture.png" in TileMapWaterMeshTest

--- tests/LabyrinthOfLore/Rendering/TileMapWaterMeshTest.cpp
+++ tests/LabyrinthOfLore/Rendering/TileMapWaterMeshTest.cpp
@@ -6,6 +6,9 @@
 #include <LabyrinthOfLore/WorldMap/TileMapLoader.hpp>
 #include <LabyrinthOfLore/WorldMap/TileTypeEnum.hpp>
 
+#define TEST_TEXTURE_FILENAME "/Users/markoates/Repos/LabyrinthOfLore/bin/programs/data/bitmaps/3x3_test_texture.png"
+
+
 TEST(LabyrinthOfLore_Rendering_TileMapWaterMeshTest, can_be_created_without_blowing_up)
 {
    LabyrinthOfLore::Rendering::TileMapWaterMesh tile_map_mesh;
@@ -28,9 +31,9 @@ TEST(LabyrinthOfLore_Rendering_TileMapWaterMeshTest, build__with_a_single_cube__
    al_init_image_addon();
 
    Tileo::TileAtlas tile_atlas;
-   ALLEGRO_BITMAP *tile_atlas_bitmap = al_load_bitmap("/Users/markoates/Repos/LabyrinthOfLore/bin/programs/data/bitmaps/grid-texture-128.png");
+   ALLEGRO_BITMAP *tile_atlas_bitmap = al_load_bitmap(TEST_TEXTURE_FILENAME);
    ASSERT_NE(nullptr, tile_atlas_bitmap);
-   tile_atlas.load(tile_atlas_bitmap, 128/3, 128/3, 0);
+   tile_atlas.load(tile_atlas_bitmap, al_get_bitmap_width(tile_atlas_bitmap)/3, al_get_bitmap_height(tile_atlas_bitmap)/3, 0);
 
    LabyrinthOfLore::Rendering::TileTypeDictionary tile_type_dictionary;
 
@@ -51,9 +54,9 @@ TEST(LabyrinthOfLore_Rendering_TileMapWaterMeshTest, build__with_a_non_water_til
    al_init_image_addon();
 
    Tileo::TileAtlas tile_atlas;
-   ALLEGRO_BITMAP *tile_atlas_bitmap = al_load_bitmap("/Users/markoates/Repos/LabyrinthOfLore/bin/programs/data/bitmaps/grid-texture-128.png");
+   ALLEGRO_BITMAP *tile_atlas_bitmap = al_load_bitmap(TEST_TEXTURE_FILENAME);
    ASSERT_NE(nullptr, tile_atlas_bitmap);
-   tile_atlas.load(tile_atlas_bitmap, 128/3, 128/3, 0);
+   tile_atlas.load(tile_atlas_bitmap, al_get_bitmap_width(tile_atlas_bitmap)/3, al_get_bitmap_height(tile_atlas_bitmap)/3, 0);
 
    LabyrinthOfLore::Rendering::TileTypeDictionary tile_type_dictionary;
 
